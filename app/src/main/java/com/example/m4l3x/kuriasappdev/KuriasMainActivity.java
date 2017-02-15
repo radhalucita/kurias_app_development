1cfe968f69094109a31f6d5901da237295b44621
dwikikusuma <kuliah.dwiki@gmail.com>
2017-02-15 23:23:45 +0700

    Mempersiapkan splashscreen

---
+++ app/src/main/java/com/example/m4l3x/kuriasappdev/KuriasMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.m4l3x.kuriasappdev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KuriasMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kurias_main);
+    }
+}
