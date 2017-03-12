61c41a2d1342cd61b5869294c4e81a1a7bd89c77
dwikikusuma <kuliah.dwiki@gmail.com>
2017-03-12 15:17:03 +0700

    Merge branch 'master' of /home/m4l3x/AndroidStudioProjects/kurias_app_development with conflicts.

--- app/src/main/java/com/example/m4l3x/kuriasappdev/KuriasMainActivity.java
+++ app/src/main/java/com/example/m4l3x/kuriasappdev/KuriasMainActivity.java
@@ -1,27 +1,95 @@
 package com.example.m4l3x.kuriasappdev;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.ArrayList;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class KuriasMainActivity extends AppCompatActivity {
 
-
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Beranda","Events","Gallery","Profile"};
+    int Numboftabs =4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kurias_main);
+
+        // Creating The Toolbar and setting it as the Toolbar for the activity
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
+
+
+    }
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+
+
+
+
+//        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+//        ChangeColorTab ccT = (ChangeColorTab) findViewById(R.id.tabChangeColorTab);
+//        ccT.setViewpager(mViewPager);
+//
+//        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+//        vpPager.setAdapter(adapterViewPager);
+//        mViewPager.setAdapter();
+
+
 
 
 
@@ -36,7 +104,7 @@ public class KuriasMainActivity extends AppCompatActivity {
 
 
 
-}
+
 
 
 
